645169a7e52b18b7c081be4b2a703327e14a757f
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-04-26 23:56:16 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { fetchCars } from "../utils";
+import { HomeProps } from "../types";
+import { fuels, yearsOfProduction } from "../constants";
 import {
   CarCard,
   ShowMore,
@@ -5,10 +8,6 @@ import {
   CustomFilter,
   Hero,
 } from "../components";
-import { fuels, yearsOfProduction } from "@/constants";
-import { HomeProps } from "@/types";
-import { fetchCars } from "@/utils";
-import Image from "next/image";
 
 export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
@@ -20,45 +19,45 @@ export default async function Home({ searchParams }: HomeProps) {
   });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
 
-      <div>
-        <div className="mt-12 padding-x max-width" id="discover">
-          <div className="home__text-container">
-            <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
-            <p>Explore out cars you might like</p>
-          </div>
-
-          <div className="home__filters">
-            <SearchBar />
-
-            <div className="home__filter-container">
-              <CustomFilter title="fuel" options={fuels} />
-              <CustomFilter title="year" options={yearsOfProduction} />
-            </div>
-          </div>
-
-          {!isDataEmpty ? (
-            <section>
-              <div className="home__cars-wrapper">
-                {allCars?.map((car) => (
-                  <CarCard car={car} />
-                ))}
-              </div>
-
-              <ShowMore
-                pageNumber={(searchParams.limit || 10) / 10}
-                isNext={(searchParams.limit || 10) > allCars.length}
-              />
-            </section>
-          ) : (
-            <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-            </div>
-          )}
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
